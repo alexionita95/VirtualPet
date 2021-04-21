8fc6ad31557edf3db6be16082a8dbbace85be338
alexionita95 <ionita.alexandru95@gmail.com>
2021-04-21 15:37:10 +0300

    Initial commit

---
+++ VirtualPet/MainForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VirtualPet
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NativeMethods.EnumWindows(delegate (IntPtr hWnd, int lParam)
+            {
+
+                if (hWnd == Handle)
+                {
+                    return true;
+                }
+                NativeMethods.TITLEBARINFO info = new NativeMethods.TITLEBARINFO();
+                NativeMethods.GetTitleBarInfo(hWnd, ref info);
+                if (!NativeMethods.IsWindowVisible(hWnd))
+                {
+                    return true;
+                }
+                StringBuilder sb = new StringBuilder(128);
+
+                NativeMethods.GetWindowText(hWnd, sb, 128);
+                if(sb.Length ==0)
+                {
+                    return true;
+                }
+                NativeMethods.RECT rect = new NativeMethods.RECT();
+                NativeMethods.GetWindowRect(new System.Runtime.InteropServices.HandleRef(this, hWnd),out rect);
+                Console.WriteLine($"{sb.ToString()} Maximized:{NativeMethods.IsZoomed(hWnd)}  {rect.Top} {rect.Left} {rect.Right} {rect.Bottom}");
+
+
+                return true;
+            }, IntPtr.Zero);
+
+            
+        }
+    }
+}
---
+++ VirtualPet/PetForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VirtualPet
+{
+    public partial class PetForm : Form
+    {
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+
+                cp.ExStyle |= 0x00000080; // WS_EX_TOOLWINDOW             <- remove from ALT-TAB list
+                cp.ExStyle |= 0x00000008; // WS_EX_TOPMOST                <- set on TopMost
+                cp.ExStyle |= 0x00080000; // WS_EX_LAYERED                <- increase paint performance
+                //cp.ExStyle |= 0x00000020; // WS_EX_TRANSPARENT            <- Do not draw window -> unclickable
+                //cp.Style |= 0x80000000; // WS_POPUP
+
+                if (Name.IndexOf("child") == 0)
+                {
+                    cp.ExStyle |= 0x08000000;   //WS_EX_NOACTIVATE  <- prevent focus when created
+                }
+                return cp;
+            }
+        }
+
+        /// <summary>
+        /// With this overridden function, it is possible to prevent the form to get the focus once created.
+        /// </summary>
+        protected override bool ShowWithoutActivation
+        {
+            get { return true; }
+        }
+        public PetForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
